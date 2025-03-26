7dc70ed62d0e52489650466c89ced4f7f6df9c99
Hilaire Sevn <portugalcapitalglobal@gmail.com>
2025-03-26 15:57:28 +0100

    Update login.js

--- login.js
+++ login.js
@@ -4,16 +4,15 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Vérification avec fichier texte
-    fetch('users.txt')
+    fetch('data.txt')
         .then(response => response.text())
         .then(data => {
-            const users = data.split('\n').map(line => line.split(','));
+            const users = data.split('\n').map(line => line.split('|'));
             const user = users.find(u => u[0] === username && u[1] === password);
 
             if (user) {
-                const [username, , role, phone, manager] = user;
-                localStorage.setItem('currentUser', JSON.stringify({ username, role, phone, manager }));
+                const [name, balance, history] = user;
+                localStorage.setItem('currentUser', JSON.stringify({ name, balance, history }));
                 window.location.href = 'dashboard.html';
             } else {
                 document.getElementById('loginMessage').textContent = 'Identifiants incorrects';
