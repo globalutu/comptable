0845b87f17dbcd558dbc5c5a6b4ef723bcca8cae
Hilaire Sevn <portugalcapitalglobal@gmail.com>
2025-03-26 15:38:26 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,22 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
+document.getElementById('loginForm').addEventListener('submit', function (e) {
     e.preventDefault();
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Simuler la lecture d'un fichier texte pour les utilisateurs
+    // Vérification avec fichier texte
     fetch('users.txt')
         .then(response => response.text())
         .then(data => {
             const users = data.split('\n').map(line => line.split(','));
-            const userFound = users.find(user => user[0] === username && user[1] === password);
+            const user = users.find(u => u[0] === username && u[1] === password);
 
-            if (userFound) {
-                localStorage.setItem('currentUser', JSON.stringify({ username }));
+            if (user) {
+                const [username, , role, phone, manager] = user;
+                localStorage.setItem('currentUser', JSON.stringify({ username, role, phone, manager }));
                 window.location.href = 'dashboard.html';
             } else {
-                document.getElementById('loginMessage').textContent = 'Nom d\'utilisateur ou mot de passe incorrect.';
+                document.getElementById('loginMessage').textContent = 'Identifiants incorrects';
             }
         });
 });
